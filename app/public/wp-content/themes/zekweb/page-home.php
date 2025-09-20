0a549e9782b77b8e5ad80fa024ec9e6aed6fd0e6
newit-danhpv <danhpv@newit.co.jp>
2025-09-20 22:40:52 +0700

    page_event

--- app/public/wp-content/themes/zekweb/page-home.php
+++ app/public/wp-content/themes/zekweb/page-home.php
@@ -1082,5 +1082,96 @@
         </div>
     </section>
     <!-- End register -->
+    <section class="news-list">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-md-4">
+                    <div class="news-card">
+                        <div class="news-thumb">
+                            <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Rasson-Woft-sieu-pham-nhap-Trung.png"
+                                alt="Rasson Woft nhập Trung" class="img-fluid">
+                        </div>
+                        <div class="news-content p-3">
+                            <h5 class="news-title">
+                                Rasson Woft nhập Trung – Phiên bản hoàn hảo từ Quang Huy Billiards
+                            </h5>
+                            <p class="news-date">03/08/2025</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="news-card">
+                        <div class="news-thumb">
+                            <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Rasson-Woft-sieu-pham-nhap-Trung.png"
+                                alt="3 mẫu bàn nhập Trung mới nhất" class="img-fluid">
+                        </div>
+                        <div class="news-content p-3">
+                            <h5 class="news-title">
+                                Giới thiệu 3 mẫu bàn nhập Trung mới nhất 2025 tại Quang Huy Billiards
+                            </h5>
+                            <p class="news-date">02/08/2025</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="news-card">
+                        <div class="news-thumb">
+                            <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Rasson-Woft-sieu-pham-nhap-Trung.png"
+                                alt="Các mẫu bàn bida Aileex chính hãng" class="img-fluid">
+                        </div>
+                        <div class="news-content p-3">
+                            <h5 class="news-title">
+                                Các mẫu bàn bida Aileex chính hãng mới nhất 2025
+                            </h5>
+                            <p class="news-date">03/02/2025</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="news-card">
+                        <div class="news-thumb">
+                            <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Rasson-Woft-sieu-pham-nhap-Trung.png"
+                                alt="Rasson Woft nhập Trung" class="img-fluid">
+                        </div>
+                        <div class="news-content p-3">
+                            <h5 class="news-title">
+                                Rasson Woft nhập Trung – Phiên bản hoàn hảo từ Quang Huy Billiards
+                            </h5>
+                            <p class="news-date">03/08/2025</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="news-card">
+                        <div class="news-thumb">
+                            <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Rasson-Woft-sieu-pham-nhap-Trung.png"
+                                alt="3 mẫu bàn nhập Trung mới nhất" class="img-fluid">
+                        </div>
+                        <div class="news-content p-3">
+                            <h5 class="news-title">
+                                Giới thiệu 3 mẫu bàn nhập Trung mới nhất 2025 tại Quang Huy Billiards
+                            </h5>
+                            <p class="news-date">02/08/2025</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="news-card">
+                        <div class="news-thumb">
+                            <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Rasson-Woft-sieu-pham-nhap-Trung.png"
+                                alt="Các mẫu bàn bida Aileex chính hãng" class="img-fluid">
+                        </div>
+                        <div class="news-content p-3">
+                            <h5 class="news-title">
+                                Các mẫu bàn bida Aileex chính hãng mới nhất 2025
+                            </h5>
+                            <p class="news-date">03/02/2025</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </main>
 <?php get_footer(); ?>
