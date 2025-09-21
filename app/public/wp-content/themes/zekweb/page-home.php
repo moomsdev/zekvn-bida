2ad9ae85532af227c6ed525b47548a6bd160ca54
newit-danhpv <danhpv@newit.co.jp>
2025-09-21 15:19:41 +0700

    project

--- app/public/wp-content/themes/zekweb/page-home.php
+++ app/public/wp-content/themes/zekweb/page-home.php
@@ -5,6 +5,76 @@
  ?>
 <?php get_header(); ?>
 <main id="main">
-    
+    <section class="posts-timeline">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-12">
+                    <div class="timeline-list">
+                        <article class="timeline-item">
+                            <div class="timeline-left">
+                                <div class="date">29/08/2025</div>
+                            </div>
+
+                            <div class="timeline-right">
+                                <div class="post-card">
+                                    <div class="post-thumb">
+                                        <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Hoan-thanh-lap-dat-ban-giao-1-ban-bi-a-tai-Tochigi-Nhat-Ban.png"
+                                            alt="">
+                                    </div>
+                                    <div class="post-body">
+                                        <div class="categories-dropdown">
+                                            <span class="categories-label">Categories ▾
+                                            <ul class="dropdown-menu">
+                                                <li><a href="#">Dự án</a></li>
+                                                <li><a href="#">Tin tức</a></li>
+                                            </ul>
+                                            </span>
+                                        </div>
+                                        <h3 class="post-title">Hoàn thành giao hàng và lắp đặt 1 bàn bi a tại Tochigi
+                                            Nhật Bản</h3>
+                                        <div class="post-meta">
+                                            <span class="comments">💬 0</span>
+                                            <a class="readmore" href="#">Xem thêm</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+
+                        <article class="timeline-item">
+                            <div class="timeline-left">
+                                <div class="date">29/08/2025</div>
+                            </div>
+
+                            <div class="timeline-right">
+                                <div class="post-card">
+                                    <div class="post-thumb">
+                                        <img src="https://quanghuybilliards.com/wp-content/uploads/2025/08/Hoan-thanh-lap-dat-ban-giao-1-ban-bi-a-tai-Tochigi-Nhat-Ban.png"
+                                            alt="">
+                                    </div>
+                                    <div class="post-body">
+                                        <div class="categories-dropdown">
+                                            <span class="categories-label">Categories ▾
+                                            <ul class="dropdown-menu">
+                                                <li><a href="#">Dự án</a></li>
+                                                <li><a href="#">Tin tức</a></li>
+                                            </ul>
+                                            </span>
+                                        </div>
+                                        <h3 class="post-title">Lắp đặt bàn bi-a cho homestay tại Hà Nội</h3>
+                                        <div class="post-meta">
+                                            <span class="comments">💬 0</span>
+                                            <a class="readmore" href="#">Xem thêm</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </main>
 <?php get_footer(); ?>
